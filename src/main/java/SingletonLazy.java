564fb36b11ed6ca20622a53aa455f1ef87245afb
leofab <leofabr3@gmail.com>
2023-03-28 14:47:27 -0300

    first commit

---
+++ src/main/java/SingletonLazy.java
@@ -0,0 +1,19 @@
+/**
+ * Singleton LAZY
+ *
+ * @autor leofab
+ */
+public class SingletonLazy {
+
+    private static SingletonLazy lazy;
+
+    private SingletonLazy(){
+        super();
+    }
+    public static SingletonLazy getInstance(){
+        if(lazy == null){
+            lazy = new SingletonLazy();
+        }
+        return lazy;
+    }
+}
