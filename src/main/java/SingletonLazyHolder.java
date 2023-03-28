3320064e80f3d504c44a0d08ccbc895a2a617923
leofab <leofabr3@gmail.com>
2023-03-28 14:52:34 -0300

    LazyHolder class implement

---
+++ src/main/java/SingletonLazyHolder.java
@@ -0,0 +1,18 @@
+/**
+ * Singleton LAZYHOLDER
+ *
+ * @autor leofab
+ */
+public class SingletonLazyHolder {
+
+    private static class Holder {
+        private static SingletonLazyHolder lazyHolder = new SingletonLazyHolder();;
+    }
+
+    private SingletonLazyHolder(){
+        super();
+    }
+    public static SingletonLazyHolder getInstance(){
+        return Holder.lazyHolder;
+    }
+}
